3b11e9296cb9f00347606513cb52e7c755149890
mohamed magdy <prof.mohamedmagdy@gmail.com>
2019-04-12 16:11:06 +0300

    Update DateTimeUtils.java

--- src/com/datazord/DateTimeUtils.java
+++ src/com/datazord/DateTimeUtils.java
@@ -1,9 +1,13 @@
 package com.datazord;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class DateTimeUtils {
 
@@ -20,5 +24,23 @@ public class DateTimeUtils {
 
 	public static LocalDateTime convertDateToLocalDateTime(Date date) {
 		return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+	}
+
+	public static String formateDate(Date date, String formatPattern) {
+		if (StringUtils.isBlank(formatPattern))
+			return date.toString();
+
+		SimpleDateFormat formatter = new SimpleDateFormat(formatPattern);
+
+		return formatter.format(new Date());
+	}
+
+	public static String formateLocalDateTime(LocalDateTime localDateTime, String formatPattern) {
+		if (StringUtils.isBlank(formatPattern))
+			return localDateTime.toString();
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(formatPattern);
+
+		return localDateTime.format(formatter);
 	}
 }
