da3ddf2bbdf9f6e7b11f09c6826338f334835552
mohamed magdy <prof.mohamedmagdy@gmail.com>
2019-07-05 15:13:15 +0300

    enhance get IP from request method

--- src/com/datazord/HttpUtils.java
+++ src/com/datazord/HttpUtils.java
@@ -2,18 +2,42 @@ package com.datazord;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class HttpUtils {
+
+	private static final String[] IP_HEADER_CANDIDATES = {
+			"X-FORWARDED-FOR",
+			"Proxy-Client-IP",
+			"WL-Proxy-Client-IP",
+			"HTTP_X_FORWARDED_FOR",
+			"HTTP_X_FORWARDED",
+			"HTTP_X_CLUSTER_CLIENT_IP",
+			"HTTP_CLIENT_IP",
+			"HTTP_FORWARDED_FOR",
+			"HTTP_FORWARDED",
+			"HTTP_VIA",
+			"REMOTE_ADDR" };
 
 	public static String getIpAddressFromRequest(HttpServletRequest request) {
 
-		String remoteAddr = "";
+		String remoteAddr = null;
 
-		if (request != null) {
-			remoteAddr = request.getHeader("X-FORWARDED-FOR");
+		if (request == null)
+			return null;
 
-			if (remoteAddr == null || "".equals(remoteAddr))
-				remoteAddr = request.getRemoteAddr();
+		for (String header : IP_HEADER_CANDIDATES) {
+			String ip = request.getHeader(header);
+
+			if (StringUtils.isBlank(ip) || ip.equalsIgnoreCase("unknown"))
+				continue;
+
+			remoteAddr = ip;
+			break;
 		}
+
+		if(remoteAddr.contains("."))
+			remoteAddr = remoteAddr.split("\\s*,\\s*")[0];
 
 		return remoteAddr;
 	}
